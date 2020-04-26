f3e95ffeb87a0708392f53614406f9d980d14e85
kumari-shilpi <kumari.shilpi@mantralabsglobal.com>
2020-04-26 14:51:04 +0530

    Optional Button Fixed, Radiobutton, Checkboxes replaced with syncfusion components

--- src/components/Cards/SelectOptionCard/RadioSelect.tsx
+++ src/components/Cards/SelectOptionCard/RadioSelect.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './RadioSelect.css';
 import Optional from '../../Optional/Optional';
 import { CheckBoxComponent } from '@syncfusion/ej2-react-buttons';
+import { RadioButtonComponent } from '@syncfusion/ej2-react-buttons';
 
 type SelectProps ={
     isRadio:boolean;
@@ -9,25 +10,10 @@ type SelectProps ={
 
 }
 
-const iconStyle = {
-      color: '#bababa',
-      fontSize:'18px',
-     
-      backgroundColor:'#666666',
-      borderRadius:'80%'
-};
-
 const closebuttonStyle ={
     color:'#999999'
 };
 
-const myStyle ={
-    color:'white',
-    border:'1px solid #999999',
-    borderRadius:'50%',
-    fontSize:'12px',
-   
-};
 const RadioSelect: React.FC<SelectProps> = ({isRadio, isCheckbox}:SelectProps) => {
     return(
         <div className='radio-select-card'>
@@ -40,77 +26,57 @@ const RadioSelect: React.FC<SelectProps> = ({isRadio, isCheckbox}:SelectProps) =
                 <ul>
                     <li>
                     <div className='item'>
-                        <div className='list-style'>
-                            {isCheckbox?<div className='checkbox' id='checkbox'>
-                                <CheckBoxComponent label="Right Side Label" checked={true}/>
+                    {isCheckbox?<div className='checkbox' id='checkbox'>
+                                    <CheckBoxComponent label="Base Plate" checked={false} cssClass='e-success e-small'/>
+                                </div>
+                                :isRadio? 
+                                <RadioButtonComponent label="Base Plate" cssClass="radio e-small" name="custom"/>
+                            :<CheckBoxComponent label="Base Plate" cssClass="e-custom" checked={true} />}
+                            
+                        <div className='close-button'>
+                        <span className='material-icons' style={closebuttonStyle}>close</span>
+                        </div>
+                    </div>
+                    </li>
+                    <li>
+                        <div className='item'>
+                        {isCheckbox?<div className='checkbox' id='checkbox'>
+                                        <CheckBoxComponent label="Dutch Mesh Assembly" checked={false} cssClass='e-success e-small'/>
+                                    </div>
+                                    :isRadio? 
+                                    <RadioButtonComponent label="Dutch Mesh Assembly" cssClass="radio e-small" name="custom"/>
+                                :<CheckBoxComponent label="Dutch Mesh Assembly" cssClass="e-custom" checked={true}/>}
+                                
+                            <div className='close-button'>
+                            <span className='material-icons' style={closebuttonStyle}>close</span>
                             </div>
-                            :isRadio? 
-                                <span className='material-icons' style={myStyle}>fiber_manual_record</span>
-                            :<span className='material-icons' style={iconStyle}>check_circle</span>
-                            
-                            }
-                            
                         </div>
-                        <div className='option-description'>
-                        Base Plate
-                        </div>
-                        <div className='close-button'>
-                        <span className='material-icons' style={closebuttonStyle}>close</span>
-                        </div>
-                    </div>
+                   
+                    
                     </li>
                     <li>
                     <div className='item'>
-                        <div className='list-style'>
-                            {isCheckbox?<div className='checkbox'></div>
-                            :isRadio? 
-                            <span className='material-icons' style={myStyle}>fiber_manual_record</span>
-                            :
-                            <span className='material-icons' style={iconStyle}>check_circle</span>
+                    {isCheckbox?<div className='checkbox' id='checkbox'>
+                                    <CheckBoxComponent label="Support Cross" checked={false} cssClass='e-success e-small'/>
+                                </div>
+                                :isRadio? 
+                                <RadioButtonComponent label="Support Cross" cssClass="radio e-small" name="custom"/>
+                            :<CheckBoxComponent label="Support Cross" cssClass="e-custom" checked={true}/>}
                             
-                            }
-                        </div>
-                        <div className='option-description'>
-                        Dutch mesh Assembly
-                        </div>
-                        <div className='close-button'>
-                        <span className='material-icons' style={closebuttonStyle}>close</span>
-                        </div>
-                    </div>
-                    </li>
-                    <li>
-                    <div className='item'>
-                        <div className='list-style'>
-                            {isCheckbox?<div className='checkbox'></div>
-                            :isRadio? 
-                            <span className='material-icons' style={myStyle}>fiber_manual_record</span>
-                            :
-                            <span className='material-icons' style={iconStyle}>check_circle</span>
-                            
-                            }
-                        </div>
-                        <div className='option-description'>
-                        Support cross
-                        </div>
                         <div className='close-button'>
                         <span className='material-icons' style={closebuttonStyle}>close</span>
                         </div>
                     </div>
                     </li>
                      <li>
-                    <div className='item'>
-                        <div className='list-style'>
-                            {isCheckbox?<div className='checkbox'></div>
-                            :isRadio? 
-                            <span className='material-icons' style={myStyle}>fiber_manual_record</span>
-                            :
-                            <span className='material-icons' style={iconStyle}>check_circle</span>
+                     <div className='item'>
+                    {isCheckbox?<div className='checkbox' id='checkbox'>
+                                    <CheckBoxComponent label="View glass(es)" checked={false} cssClass='e-success e-small'/>
+                                </div>
+                                :isRadio? 
+                                <RadioButtonComponent label="View glass(es)" cssClass="radio e-small" name="custom"/>
+                            :<CheckBoxComponent label="View glass(es)" cssClass="e-custom" checked={true}/>}
                             
-                            }
-                        </div>
-                        <div className='option-description'>
-                        View glass(es)
-                        </div>
                         <div className='close-button'>
                         <span className='material-icons' style={closebuttonStyle}>close</span>
                         </div>
@@ -118,18 +84,12 @@ const RadioSelect: React.FC<SelectProps> = ({isRadio, isCheckbox}:SelectProps) =
                     </li>
                     <li>
                     <div className='item'>
-                        <div className='list-style'>
-                            {isCheckbox?<div className='checkbox'></div>
-                            :isRadio? 
-                            <span className='material-icons' style={myStyle}>fiber_manual_record</span>
-                            :
-                            <span className='material-icons' style={iconStyle}>check_circle</span>
-                            
-                            }
-                        </div>
-                        <div className='option-description'>
-                        Fluid
-                        </div>
+                    {isCheckbox?<div className='checkbox' id='checkbox'>
+                                    <CheckBoxComponent label="Fluid" checked={false} cssClass='e-success e-small'/>
+                                </div>
+                                :isRadio? 
+                                <RadioButtonComponent label="Fluid" cssClass="radio e-small" name="custom"/>
+                                :<CheckBoxComponent label="Fluid" cssClass="e-custom" checked={true}/>     }                     
                         <div className='close-button'>
                         <span className='material-icons' style={closebuttonStyle}>close</span>
                         </div>
