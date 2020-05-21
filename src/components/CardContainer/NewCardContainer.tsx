44088f48dcaa04450154999e803c26ceecc634cd
kumari-shilpi <kumari.shilpi@mantralabsglobal.com>
2020-05-21 15:45:59 +0530

    All changes made as per changes in design

--- src/components/CardContainer/NewCardContainer.tsx
+++ src/components/CardContainer/NewCardContainer.tsx
@@ -13,11 +13,32 @@ type CardProps = {
 const NewCardContainer = ({add_stop,due_on,timed,optional, question, com_arr}:CardProps) => {
     return(
         <div className='new-card-container'>
-            <div className='step-description-card'>
-                <StepDescription question={question} add_stop={add_stop} due_on={due_on} timed={timed} optional={optional}/>
+            <div className='question-section'>
+                <div className='step-description-card'>
+                    <StepDescription question={question} add_stop={add_stop} due_on={due_on} timed={timed} optional={optional}/>
+                </div>
+                <div className='add-button'>
+                    <span className='material-icons' style={{color:'#999999',fontSize:'32px'}}>add_circle</span>
+                </div>
+
             </div>
-            <div className='components-card'>
-            {com_arr.map((item:any)=><div style={{listStyle:'none', paddingBottom:'20px'}}>{item}</div>)}
+            <div className='components-section'>
+                
+                    {com_arr.map((item:any)=>
+                    
+                    <div className='component-card'>
+                        <div style={{listStyle:'none', paddingBottom:'20px'}}>
+                            {item}
+                        </div>
+                        <div className='three-dots'>
+                            <span className="material-icons" style={{fontSize:'38px',color:'#999999'}}>
+                                more_vert
+                            </span>
+                        </div>
+                </div>)}
+            
+                
+
             </div>
 
         </div>
